7e3818bf09a9557b12be30313c0808079f771a3f
xb <xb000111@gmail.com>
2021-12-16 18:17:20 +0400

    hdhhil

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+
+    private String name = "fhdhdh";
+    private int age;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static int jk(int k){
+
+        return k*5;
+
+    }
+    public static void j(){
+
+        System.out.println(jk(20));
+    }
+
+}
